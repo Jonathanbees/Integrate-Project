0f0df0d53ad1f8c524da28a37f07717741194690
Jonathanbees <jonathanbetancurespinosa@gmail.com>
2023-08-13 21:20:01 -0500

    Template adaptation

--- onlinestore/views.py
+++ onlinestore/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'onlinestore/index.html')
+
+def contact(request):
+    return render(request,'onlinestore/contact.html')
+def about(request):
+    return render(request,'onlinestore/about.html')
+def shop(request):
+    return render(request,'onlinestore/shop.html')
--- proyecto/urls.py
+++ proyecto/urls.py
@@ -24,6 +24,9 @@ from onlinestore import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('shop/', views.shop, name='shop'),
 ]
 urlpatterns += staticfiles_urlpatterns()
